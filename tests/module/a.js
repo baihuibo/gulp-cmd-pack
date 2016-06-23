f675f08e614e904dade5fc24ee94414681410c2f
baihuibo <baihuibo@hotmail.com>
2016-06-23 18:26:28 +0800

    调整测试例子代码

--- tests/module/a.js
+++ tests/module/a.js
@@ -2,7 +2,10 @@
  * Created by 惠波 on 2015/7/31.
  */
 
+var deps = [require('./b')];
+var deps2 = (require('./b'));
 var b = require('./b');
+var tpl = require('./test.ejs');
 
 exports.test = function () {
     console.log('module b : ' + b);
