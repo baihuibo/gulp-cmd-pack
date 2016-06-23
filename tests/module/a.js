0eef17445412c37c39f17964e5066955716fb17c
baihuibo <baihuibo@hotmail.com>
2016-06-23 18:34:04 +0800

    调整测试例子代码

--- tests/module/a.js
+++ tests/module/a.js
@@ -2,10 +2,15 @@
  * Created by 惠波 on 2015/7/31.
  */
 
-var deps = [require('./b')];
-var deps2 = (require('./b'));
+var deps = [require('./b')]; // 数组请求测试
+var deps2 = (require('./b')); // 括弧请求测试(因为多匹配一个字符导致的问题)
 var b = require('./b');
 var tpl = require('./test.ejs');
+
+var c = myrequire('mod'); //测试方法带有关键字时匹配不许通过
+function myrequire(mod) {
+    return mod;
+}
 
 exports.test = function () {
     console.log('module b : ' + b);
